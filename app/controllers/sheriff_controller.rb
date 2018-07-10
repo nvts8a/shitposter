6c76aca24a9ca7c3fd4e10ea0405f2b66f1e4e49
Steven Justin <nevets138@gmail.com>
2018-07-09 20:55:12 -0500

    Body fixin

--- app/controllers/sheriff_controller.rb
+++ app/controllers/sheriff_controller.rb
@@ -1,12 +1,12 @@
 class SheriffController < ApplicationController
   BODY = 
-".         :babby:\n" +
+".           :babby:\n" +
 ".      :100::100::100:\n" +
-".    :100:  :100:  :100:\n" +
+".    :100:   :100:   :100:\n" +
 ".    :point_down: :100::100: :point_down:\n" +
-".        :100:  :100:\n" +
-".        :100:  :100:\n" +
-".        :boot:  :boot:\n"
+".        :100:    :100:\n" +
+".        :100:     :100:\n" +
+".        :boot:     :boot:\n"
 
   def index
     render json: {}
