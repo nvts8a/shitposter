5fbc0b4ebb209cf2ace72516c30c04e77972435e
Steven Justin <nevets138@gmail.com>
2018-07-09 21:35:50 -0500

    message stuff

--- app/controllers/sheriff_controller.rb
+++ app/controllers/sheriff_controller.rb
@@ -25,8 +25,14 @@ class SheriffController < ApplicationController
     head = text.shift
     subtext = ""
     if text.any?
-      rest = text.join(" ")
-      subtext = "\nhowdy. im the sheriff of " + rest + ". im gon " + rest
+      rest1 = text.join(" ")
+      rest1.downcase!
+      rest2 = rest1
+      rest2.gsub! 'ing ', ' '
+      rest2.gsub! "in\' ", ' '
+      rest2.gsub! 'in ', ' '
+      
+      subtext = "\nhowdy. im the sheriff of " + rest1 + ". im gon " + rest2
     end
 
     HEADSPACE + head + BODY + subtext 
